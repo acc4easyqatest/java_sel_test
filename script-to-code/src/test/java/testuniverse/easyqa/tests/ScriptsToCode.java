005b197e7110a9e2793e9a18d35ac3af3abbae80
acc4easyqatest <acc4easyqatest@gmail.com>
2017-06-13 02:24:23 +0300

    рефакторинг - выделение параметров

--- script-to-code/src/test/java/testuniverse/easyqa/tests/ScriptsToCode.java
+++ script-to-code/src/test/java/testuniverse/easyqa/tests/ScriptsToCode.java
@@ -3,16 +3,11 @@ package testuniverse.easyqa.tests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class ScriptsToCode {
     FirefoxDriver wd;
@@ -25,23 +20,25 @@ public class ScriptsToCode {
     
     @Test
     public void ScriptsToCode() {
-        userLogin();
+        //вызываем логин с переданным паролем и логином
+        userLogin("acc4easyqatest@gmail.com", "acc4easyqatestQwe");
         gotoProjectsList();
         selectProject();
         gotoBugTracker();
-        createCardOnTracker();
+        //создание карточки теперь тоже параметризированное
+        createCardOnTracker("карточка 3", "описание карточки 3");
 
     }
 
-    public void createCardOnTracker() {
+    public void createCardOnTracker(String cardName, String cardDescription) {
         //создаем карточку
         wd.findElement(By.linkText("Создать карточку")).click();
         //название карточки
         wd.findElement(By.id("issue_summary")).click();
-        wd.findElement(By.id("issue_summary")).sendKeys("карточка 3");
+        wd.findElement(By.id("issue_summary")).sendKeys(cardName);
         //описание карточки
         wd.findElement(By.id("issue_description")).click();
-        wd.findElement(By.id("issue_description")).sendKeys("описание карточки 3");
+        wd.findElement(By.id("issue_description")).sendKeys(cardDescription);
         //выбираем тип и приоритет
         /*
         *пока спрячем, потому что добраться тяжело
@@ -69,15 +66,16 @@ public class ScriptsToCode {
         wd.findElement(By.linkText("Мои Проекты")).click();
     }
 
-    public void userLogin() {
+    public void userLogin(String userLogin, String userPassword) {
+
         wd.get("https://app.geteasyqa.com/users/sign_in");
         //вводим емейл
         wd.findElement(By.id("user_email")).click();
-        wd.findElement(By.id("user_email")).sendKeys("acc4easyqatest@gmail.com");
+        wd.findElement(By.id("user_email")).sendKeys(userLogin);
 
         //вводим пароль
         wd.findElement(By.id("user_password")).click();
-        wd.findElement(By.id("user_password")).sendKeys("acc4easyqatestQwe");
+        wd.findElement(By.id("user_password")).sendKeys(userPassword);
 
         //нажимаем на кнопку войти
         wd.findElement(By.name("commit")).click();
