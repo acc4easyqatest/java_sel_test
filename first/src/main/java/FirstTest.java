06840c4465678ee42e1b6e7b82db38907541a52d
acc4easyqatest <acc4easyqatest@gmail.com>
2017-06-12 21:05:57 +0300

    java operations demo

--- first/src/main/java/FirstTest.java
+++ first/src/main/java/FirstTest.java
@@ -2,5 +2,12 @@ public class FirstTest {
 	
 	public static void main (String[] args) {
 		System.out.println ("We are here! Hello! ");
+
+		System.out.println (8 + 2);
+		System.out.println (8 - 2);
+		System.out.println (8 * 2);
+		System.out.println (8 / 3);
+
+		System.out.println (8.0 / 3);
 	}
 }
